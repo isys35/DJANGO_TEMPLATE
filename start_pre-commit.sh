93113e7406cf6d192ed2f3f2eb3e1e338dcbe57f
isys35 <isys35@mail.ru>
2023-04-30 23:56:19 +0300

    fix: add rename-project.yml

--- start_pre-commit.sh
+++ start_pre-commit.sh
@@ -1,3 +1,4 @@
 pre-commit install --allow-missing-config
 pre-commit install --hook-type commit-msg --allow-missing-config
-cp -rf script/local_pre-commit/post-commit .git/hooks/post-commit
+cp -rf scripts/local_pre-commit/post-commit .git/hooks/post-commit
+chmod ug+x .git/hooks/*
